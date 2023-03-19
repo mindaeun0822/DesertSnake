9ac8261e8308291414a2140e4150d28e40c29e5b
mindaeun0822 <124743722+mindaeun0822@users.noreply.github.com>
2023-03-18 19:57:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import js
 p5 = js.window
 
+
+class Snake:
+    x = 150
+    y = 150
+
+    snake_img = p5.loadImage('snake.png')
+    
+    def draw(self):
+        p5.image(self.snake_img, self.x, self.y)
+
 def setup():
     p5.createCanvas(300, 300) 
     print('finished setup') 
+
+snake = Snake()
     
 def draw():
-    p5.background(255)           
+    p5.background(255)
+    background_img = p5.loadImage('background.png')
+    p5.image(background_img,0,0)
+
+    snake.draw()
 
 def keyPressed(event):
     pass 
